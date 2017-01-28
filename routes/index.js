c1d21130b4012c628f5fe8f44775d74c79467040
Stankovic <aleksa.stankovic@berkeley.edu>
2017-01-28 10:12:52 +0100

    finished

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,43 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+router.get('/:time', function(req, res) {
+
+	function unixToNatural (unix) {
+		var date = new Date(unix * 1000);
+		var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+		return months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
+	}
+
+	var time = req.params.time;
+
+    if (!isNaN(time)) {
+    	res.json(
+    		{
+    			unix: time,
+    			natural: unixToNatural(time)
+    		}
+    	)
+    } else if (!isNaN(new Date (time))) {
+    	res.json(
+    		{
+    			unix: (new Date (time) / 1000),
+    			natural: time
+    		}
+    	)
+    } else {
+    	res.json(
+    		{
+    			unix: null,
+    			natural: null
+    		}
+    	)
+    }
+
 });
 
 module.exports = router;
